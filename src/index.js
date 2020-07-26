710cb024c695e863d24db8bd2e73ef002ee2d82b
Stonek79 <59825083+Stonek79@users.noreply.github.com>
2020-07-26 15:59:13 +0300

    Fix linter comments

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@
 
 import fs from 'fs';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 import path from 'path';
 import _ from 'lodash';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 const genDiff = (filepath1, filepath2) => {
   const getPath = (filename) => path.resolve(__dirname, '..', '_fixtures_', filename);
@@ -15,9 +14,13 @@ const genDiff = (filepath1, filepath2) => {
   const filedata2 = JSON.parse(fs.readFileSync(getPath(filepath2), 'utf-8'));
   const getEntries = (obj) => Object.entries(obj);
   const filedata1Diff = getEntries(filedata1)
-    .reduce((acc, [key, value]) => ((!_.has(filedata2, key)) ? [...acc, `  - ${key}: ${value}`]
-      : (filedata2[key] === value ? [...acc, `    ${key}: ${value}`]
-        : [...acc, `  - ${key}: ${value}`, `  + ${key}: ${filedata2[key]}`])), []);
+    .reduce((acc, [key, value]) => {
+      if (!_.has(filedata2, key)) return [...acc, `  - ${key}: ${value}`];
+      if (filedata2[key] === value) {
+        return [...acc, `    ${key}: ${value}`];
+      }
+      return [...acc, `  - ${key}: ${value}`, `  + ${key}: ${filedata2[key]}`];
+    }, []);
   const filedata2Diff = getEntries(filedata2)
     .reduce((acc, [key, value]) => (!_.has(filedata1, key) ? [...acc, `  + ${key}: ${value}`]
       : [...acc]), filedata1Diff);
