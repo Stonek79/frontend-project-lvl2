38cc8a4487d2ff024b74c7105589d0b1757fb4d1
Stonek79 <59825083+Stonek79@users.noreply.github.com>
2020-08-26 11:18:29 +0300

    Fix mentors comments
    1) rename function that added parsing data add parse process in it
    2) refactoring 'generateDifferences': 
    - deleted parsing functions;
    - rename format function

--- src/index.js
+++ src/index.js
@@ -7,23 +7,20 @@ import parse from './parsers.js';
 import getFormatter from './formatters/index.js';
 import getDifferences from './getDifferences.js';
 
-const getDataForParse = (filepath) => {
+const geParsedData = (filepath) => {
   const extention = path.extname(filepath);
   const fileData = fs.readFileSync(filepath, 'utf-8');
-  return [fileData, extention];
+  return parse(fileData, extention);
 };
 
 const generateDifferences = (filepath1, filepath2, format = 'stylish') => {
-  const dataForParse1 = getDataForParse(filepath1);
-  const dataForParse2 = getDataForParse(filepath2);
-
-  const parsedData1 = parse(...dataForParse1);
-  const parsedData2 = parse(...dataForParse2);
+  const parsedData1 = geParsedData(filepath1);
+  const parsedData2 = geParsedData(filepath2);
 
   const diff = getDifferences(parsedData1, parsedData2);
-  const formatter = getFormatter(format);
+  const makeFormat = getFormatter(format);
 
-  return formatter(diff);
+  return makeFormat(diff);
 };
 
 export default generateDifferences;
